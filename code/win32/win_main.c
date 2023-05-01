96658b83bf1dadfa4d10400ce190dd52dca8bfa4
Sancte Bruno, ora pro nobis <sanctebruno@gmail.com>
2023-05-01 18:38:55 -0300

    Fix build for mingw

--- code/win32/win_main.c
+++ code/win32/win_main.c
@@ -32,7 +32,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <sys/stat.h>
 #include <direct.h>
 #include <io.h>
-
+#include <errno.h>
 
 #define MEM_THRESHOLD (96*1024*1024)
 
